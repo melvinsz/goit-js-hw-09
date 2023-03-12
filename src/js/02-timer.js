1fce092c5bafff896c1cc7450cebe88524f0270a
Stanislav <Stas.zaliiski@gmail.com>
2023-03-12 22:06:27 +0100

    2 tasks

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
 const startRef = document.querySelector('[data-start]');
 const daysRef = document.querySelector('[data-days]');
@@ -7,34 +8,48 @@ const hoursRef = document.querySelector('[data-hours]');
 const minutesRef = document.querySelector('[data-minutes]');
 const secondsRef = document.querySelector('[data-seconds]');
 
+const addLeadingZero = value => value.toString().padStart(2, '0');
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
-    if (selectedDates[0] < new Date()) {
-      window.alert('Please choose a date in the future');
+    if (selectedDates[0] <= Date.now()) {
+      Notiflix.Notify.warning('Please choose a date in the future');
       startRef.disabled = true;
     } else startRef.disabled = false;
+    startRef.addEventListener('click', () => onStartClick(selectedDates[0]));
   },
 };
 
 flatpickr('#datetime-picker', options);
 
-startRef.addEventListener('click', onStartClick);
-
 function onStartClick(e) {
-  setInterval(() => {
-    const delta = 'Обрана дата' - Date.now();
-    console.log(delta);
+  const intervalId = setInterval(() => {
+    const delta = e.getTime() - Date.now();
+
+    if (delta <= 0) {
+      clearInterval(intervalId);
+      daysRef.textContent = '00';
+      hoursRef.textContent = '00';
+      minutesRef.textContent = '00';
+      secondsRef.textContent = '00';
+      Notiflix.Notify.success('Countdown has finished!');
+      return;
+    }
+    const { days, hours, minutes, seconds } = convertMs(delta);
+    daysRef.textContent = addLeadingZero(days);
+    hoursRef.textContent = addLeadingZero(hours);
+    minutesRef.textContent = addLeadingZero(minutes);
+    secondsRef.textContent = addLeadingZero(seconds);
   }, 1000);
 }
-
-// ___________________________________________________________________________
+// // ___________________________________________________________________________
 
 function convertMs(ms) {
+  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
