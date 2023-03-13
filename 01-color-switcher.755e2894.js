ec0149e295f9ffaafab2dfa315f642706a5d75d4
melvinsz <113125220+melvinsz@users.noreply.github.com>
2023-03-13 14:46:20 +0000

    Deploying to gh-pages from @ melvinsz/goit-js-hw-09@33b5f4c096b7fc6e0f221e6ff618338cd315b52b 🚀

--- 01-color-switcher.755e2894.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;t.addEventListener("click",(function(e){n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0})),e.addEventListener("click",(function(e){clearInterval(n),t.disabled=!1,document.body.style.backgroundColor=""}))}();
-//# sourceMappingURL=01-color-switcher.755e2894.js.map
